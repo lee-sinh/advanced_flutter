8d39a2bce749b0ec74afb52459a7ec2c37bef3c5
lee-sinh <oeurnleesinh52@gmail.com>
2025-03-06 02:30:42 +0700

    BLA-005

--- lib/W3-BLABLA-PROJECT/model/ride/locations.dart
+++ lib/W3-BLABLA-PROJECT/model/ride/locations.dart
@@ -19,6 +19,10 @@ class Location {
   final Country country;
 
   const Location({required this.name, required this.country});
+    // Copy constructor
+  Location.copy(Location other)
+    : name = other.name,
+      country = other.country;
 
   @override
   bool operator ==(Object other) {
