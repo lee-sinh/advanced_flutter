5d50b8f261c5be8860867639acdf41a3feea115f
lee-sinh <oeurnleesinh52@gmail.com>
2025-03-06 02:45:10 +0700

    refactor folder structure

---
+++ lib/BLABLA-PROJECT/main.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+import 'package:week_3_blabla_project/BLABLA-PROJECT/widgets/actions/bla_button.dart';
+// import 'widgets/actions/bla_button.dart';
+// import 'screens/ride_pref/ride_pref_screen.dart';
+import 'theme/theme.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      theme: appTheme,
+      home: Scaffold(body: BlaButton(text: '', onPressed: () {  },)),
+    );
+  }
+}
